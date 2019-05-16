b90ce16624dd65798fb64f36b5d766ed5ce28b70
Gumpsj <Gumpsj@163.com>
2019-05-17 00:43:51 +0800

    githubAblockweb
    githubAblockweb

---
+++ AblockDuinoV1.1/AblockDuino/msg/js/zh-hans_BD.js
@@ -0,0 +1,18 @@
+'use strict';
+
+goog.provide('Blockly.Msg.zh-hans_BD');
+
+goog.require('Blockly.Msg');
+
+// color definition
+Blockly.Msg.ARDUINO_BASE_HUE = 180;
+Blockly.Msg.GROVE_HUE = 120;
+Blockly.Msg.SERVO_HUE = 215;
+
+// toolbox categories name
+Blockly.Msg.CAT_ARDUINO_BASE = "输入/输出";
+Blockly.Msg.CAT_SERVO = "舵机";
+Blockly.Msg.CAT_GROVE = "数字信号传感器";
+Blockly.Msg.CAT_GROVE_ANALOG = "模拟信号传感器";
+Blockly.Msg.CAT_GROVE_LCD = "LCD液晶屏";
+Blockly.Msg.CAT_GROVE_MOTOR = "直流电机";
